2802bf3652fd3ec14bdda71cbc14e0c5560c9859
kvinkel <kvinkel@hotmail.com>
2019-03-29 16:42:11 +0100

    Create SensumInterface.java
    Interface for the basic functions of the GUI.

---
+++ src/GUI/SensumInterface.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author KV
+ */
+public interface SensumInterface {
+    
+    /**
+     * 
+     * @return Map containing UUIDs of all patients along with the associated names
+     */
+    public Map<UUID, String> getPatientList();
+    
+    /**
+     * 
+     * @param patientId id of patient from which the diaries are acquired
+     * @return Map containing dates of all diaries along with the associated description (i.e. notes)
+     */
+    public Map<Date, String> getDiaryList(UUID patientId);
+    
+    /**
+     * 
+     * @param patientId id of patient from which the civil registration number is acquired
+     * @return String containing the CPR-number
+     */
+    public String getCpr(UUID patientId);
+    
+    /**
+     * 
+     * @param patientId id of patient from which the image is acquired
+     * @return image of the patient
+     */
+    public Image getPatientImage(UUID patientId);
+    
+    /**
+     * 
+     * @param patientId id of patient from which the info is acquired
+     * @return String containing patient info
+     */
+    public String getPatientInfo(UUID patientId);
+    
+    /**
+     * 
+     * @param patientID id of patient the diary belongs to
+     * @param date date associated with the diary
+     * @return true if the save was successful
+     */
+    public Boolean saveDiary(UUID patientID, Date date);
+    
+    /**
+     * 
+     * @param patientID id of patient the diary belongs to
+     * @return the current date
+     */
+    public Date createDiary(UUID patientID);
+    
+}
