af2a32af6681249d35c3c7c72eb1c5db5f587c8e
JakobKaae <jakaa18@student.sdu.dk>
2019-04-09 12:16:47 +0200

    Added abstract methds

--- src/sensum_bosted/DomainFacade.java
+++ src/sensum_bosted/DomainFacade.java
@@ -5,13 +5,68 @@
  */
 package sensum_bosted;
 
+import GUI.SensumInterface;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  *
  * @author jakob
  */
-public class DomainFacade {
+public class DomainFacade implements SensumInterface {
     
     public static void main(String[] args) {
         
+    }
+
+    @Override
+    public String getUserName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<UUID, String> getPatientsMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public UUID initializePatient(UUID patientId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPatientName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPatientCPR() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPatientImage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPatientInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<Date, UUID> getNotationsMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public UUID initializeDiary() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNotation(UUID notationId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
