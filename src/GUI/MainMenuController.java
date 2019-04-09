7095b4ce7430c3ad7fe35dcde06a002d880d7f06
Simon Holland Flarup <mail@simonflarup.dk>
2019-04-09 14:54:57 +0200

    Running

--- src/GUI/MainMenuController.java
+++ src/GUI/MainMenuController.java
@@ -5,18 +5,26 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import sensum_bosted.DomainFacade;
 
 /**
  * FXML Controller class
@@ -41,6 +49,7 @@ public class MainMenuController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        fc = new DomainFacade();
         nameUser.setText(fc.getUserName());
         for (Map.Entry<UUID, String> entry : fc.getPatientsMap().entrySet()) {
             ListPerson lp = new ListPerson(entry.getKey(), entry.getValue());
@@ -51,6 +60,13 @@ public class MainMenuController implements Initializable {
 
     @FXML
     private void selectPatient(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/GUI/PatientMenu.fxml"));
+            Scene scene = nameUser.getScene();
+            scene.setRoot(root);
+        } catch (IOException ex) {
+            System.out.println("Error");
+        }
     }
 
 }
--- src/sensum_bosted/Sensum_Bosted.java
+++ src/sensum_bosted/Sensum_Bosted.java
@@ -20,7 +20,7 @@ public class Sensum_Bosted extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainMenu.fxml"));
         
         Scene scene = new Scene(root);
         
