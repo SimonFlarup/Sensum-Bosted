87970cc8e5d0af80cb33b306bb900cd3e82c4845
kvinkel <kvinkel@hotmail.com>
2019-04-12 14:59:52 +0200

    Minor tweak 2.0

--- src/GUI/DiaryMenuController.java
+++ src/GUI/DiaryMenuController.java
@@ -60,7 +60,6 @@ public class DiaryMenuController implements Initializable {
             notations.add(lvf);
         }
         notationList.setItems(notations);
-        notationList.getSelectionModel().select(0);
 
     }
 
