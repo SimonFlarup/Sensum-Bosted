0aee063103b5876ce4bc8b4c3948cdd1e1bb17ad
kvinkel <kvinkel@hotmail.com>
2019-04-23 10:23:57 +0200

    Update test for cpr format

--- src/GUI/CreatePatientMenuController.java
+++ src/GUI/CreatePatientMenuController.java
@@ -63,7 +63,7 @@ public class CreatePatientMenuController implements Initializable {
         if (nameField.getText().isEmpty()) {
             alert.setContentText("Intet navn angivet.");
             alert.show();
-        } else if (!cprField.getText().contains("-") || cprField.getText().length() != 11 || cprField.getText().isEmpty()) {
+        } else if (cprField.getText().length() != 11 || !cprField.getText().substring(6, 7).contains("-")) {
             alert.setContentText("Ugyldigt CPR nummer.");
             alert.show();
         } else if (infoArea.getText().isEmpty()) {
