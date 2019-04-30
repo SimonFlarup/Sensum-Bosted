4a0c6b83734ac4172f72dd98fb2545ccdb7cffda
Jakob-k <jakob.kaae1@gmail.com>
2019-04-30 12:18:31 +0200

    Implemented hashing

--- src/sensum_bosted/PasswordHashing.java
+++ src/sensum_bosted/PasswordHashing.java
@@ -9,6 +9,54 @@ package sensum_bosted;
  *
  * @author jakob
  */
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class PasswordHashing {
-    
+
+    String password = "";
+
+    MessageDigest md;
+
+    public PasswordHashing() {
+
+    }
+
+    public PasswordHashing(String password) {
+
+    }
+
+    public String hash(String password) {
+        try {
+            // Select the message digest for the hash computation -> SHA-256
+            md = MessageDigest.getInstance("SHA-256");
+
+            // Generate the random salt
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+
+            // Passing the salt to the digest for the computation
+            md.update(salt);
+
+            // Generate the salted hash
+            byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashedPassword) {
+                sb.append(String.format("%02x", b));
+            }
+
+            System.out.println(sb);
+
+            password = sb.toString();
+            return password;
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return password;
+    }
 }
