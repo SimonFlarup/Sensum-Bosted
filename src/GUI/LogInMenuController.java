71fc5551ab7928b9c525aff798e819a92f2d682e
Sebastian <semol18@student.sdu.dk>
2019-04-30 13:00:09 +0200

    Updated main menu
    Added logout button to main menu, that returns to the login menu.

--- src/GUI/LogInMenuController.java
+++ src/GUI/LogInMenuController.java
@@ -29,6 +29,8 @@ public class LogInMenuController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/GUI/MainMenuController.java
+++ src/GUI/MainMenuController.java
@@ -8,8 +8,11 @@ package GUI;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -20,9 +23,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import sensum_bosted.DomainFacade;
 
 /**
@@ -40,9 +45,12 @@ public class MainMenuController implements Initializable {
     private Button helpButton;
     @FXML
     private Button createPatientButton;
+    @FXML
+    private Button logoutButton;
 
     private SensumInterface fc;
     private ObservableList patients = FXCollections.observableArrayList();
+    private Alert alert;
 
     /**
      * Initializes the controller class.
@@ -92,6 +100,28 @@ public class MainMenuController implements Initializable {
             scene.setRoot(root);
         } catch (IOException ex) {
             System.out.println("Error");
+        }
+    }
+
+    @FXML
+    private void logout(ActionEvent event) {
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText("Vil du logge ud?");
+        alert.setTitle("Advarsel");
+        alert.setHeaderText("");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get().equals(ButtonType.OK)) {
+            try {
+                Stage currentStage = (Stage) logoutButton.getScene().getWindow();
+                currentStage.close();
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/LogInMenu.fxml"));
+                Stage stage = new Stage();
+                stage.setTitle("Sensum Bosted");
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
