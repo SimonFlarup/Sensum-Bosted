044afedee08dfdde107d97419abed68cc2dfe756
Simon Holland Flarup <mail@simonflarup.dk>
2019-04-30 22:01:30 +0200

    Hashmap prototype

--- src/storage/CRUD.java
+++ src/storage/CRUD.java
@@ -5,7 +5,11 @@
  */
 package storage;
 
+import GUI.ListViewInfo;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  *
@@ -30,7 +34,8 @@ public class CRUD {
         }
         return CRUD.instance;
     }
-/*
+
+    /*
     public void test() {
         System.out.println("Testing");
         try {
@@ -49,27 +54,39 @@ public class CRUD {
             System.out.println(e.getMessage());
         }
     }
-    */
+     */
     public void Create() {
         System.out.println("Creating");
+        Map<Enum, String> result = new HashMap<>();
         try {
             Connection db = DriverManager.getConnection(url, username, password);
             Statement st = db.createStatement();
-            st.executeUpdate("INSERT INTO \"User\" " + "VALUES ('Jonas', 'pasworld', 'Jonas Ahwazian', 'PATIENT')");
+            //st.executeUpdate("INSERT INTO \"User\" " + "VALUES ('Jonas', 'pasworld', 'Jonas Ahwazian', 'PATIENT')");
             ResultSet rs = st.executeQuery("SELECT * FROM \"User\"");
+            ResultSetMetaData rsmd = rs.getMetaData();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                System.out.println(rsmd.getColumnName(i));
+            }
             while (rs.next()) {
+                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    System.out.println(rsmd.getColumnName(i));
+                    result.put(Fields.UserFields.valueOf(rsmd.getColumnName(i).toUpperCase()), rs.getString(i));
+                }
                 System.out.print("Column 1 returned ");
                 System.out.println(rs.getString(2));
                 System.out.print("Column 2 returned ");
-                System.out.println(rs.getString(3));            
-        }
+                System.out.println(rs.getString(3));
+            }
             rs.close();
             st.close();
         } catch (java.sql.SQLException e) {
             System.out.println(e.getMessage());
-        }    
+        }
+        for (Map.Entry<Enum, String> entry : result.entrySet()) {
+            System.out.println(entry.getKey().toString() + " | " + entry.getValue());
+        }
     }
-    
+
     /*    
     //Create (C)
     public void create(Tables table, Map<Enum, String> data, User user);
@@ -78,7 +95,6 @@ public class CRUD {
     public HashMap<Enum, String>[] readAll(Tables table, User user);
     //Update (U)
     public void update(Tables table, UUID primaryKey, Map<Enum, String> data, User user); */
-    
     public static void main(String[] args) {
         CRUD.getInstance().Create();
     }
