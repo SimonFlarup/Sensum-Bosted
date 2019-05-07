b81859ef2f03da33f21102c26247f71df82b5b73
Sebastian <semol18@student.sdu.dk>
2019-05-07 10:41:03 +0200

    Start up on login screen
    Login logic needs implementing

--- src/GUI/LogInMenuController.java
+++ src/GUI/LogInMenuController.java
@@ -5,13 +5,20 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -39,7 +46,17 @@ public class LogInMenuController implements Initializable {
 
     @FXML
     private void login(ActionEvent event) {
-        
+        try {
+                Stage currentStage = (Stage) loginButton.getScene().getWindow();
+                currentStage.close();
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainMenu.fxml"));
+                Stage stage = new Stage();
+                stage.setTitle("Sensum Bosted");
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
     
 }
